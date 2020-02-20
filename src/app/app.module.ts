a07d89b66f55a01be95500baf811e5715e1707a0
hta91-cr <hta200891@gmail.com>
2020-02-20 13:08:29 -0600

    Changed logo and menu color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { AgmCoreModule } from "@agm/core";
-import { AppComponent } from "./app.component";
-import { AppRoutingModule } from "./app.routing.module";
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 import {
   MDBSpinningPreloader,
   MDBBootstrapModulesPro,
   ToastModule
-} from "ng-uikit-pro-standard";
-import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { ServiceWorkerModule } from "@angular/service-worker";
-import { environment } from "../environments/environment";
-import { NgwWowModule } from "ngx-wow";
+} from 'ng-uikit-pro-standard';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NgwWowModule } from 'ngx-wow';
 /* ---------------------------------- */
-import { MenuComponent } from "../menu/menu/menu.component";
-import { HomeComponent } from "../core/home/home.component";
-import { AboutComponent } from "../core/about/about.component";
-import { ParticlesModule } from "angular-particle";
-import { SkillsComponent } from "../core/skills/skills.component";
-import { ProjectsComponent } from "../core/projects/projects.component";
-import { ContactComponent } from "../core/contact/contact.component";
-import { MainService } from "../services/main.service";
+import { MenuComponent } from '../menu/menu/menu.component';
+import { HomeComponent } from '../core/home/home.component';
+import { AboutComponent } from '../core/about/about.component';
+import { ParticlesModule } from 'angular-particle';
+import { SkillsComponent } from '../core/skills/skills.component';
+import { ProjectsComponent } from '../core/projects/projects.component';
+import { ContactComponent } from '../core/contact/contact.component';
+import { MainService } from '../services/main.service';
 
 @NgModule({
   declarations: [
@@ -46,12 +46,12 @@ import { MainService } from "../services/main.service";
     MDBBootstrapModulesPro.forRoot(),
     AgmCoreModule.forRoot({
       // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en#key
-      apiKey: "Your_api_key"
+      apiKey: 'Your_api_key'
     }),
     AppRoutingModule,
     ReactiveFormsModule,
     ParticlesModule,
-    ServiceWorkerModule.register("ngsw-worker.js", {
+    ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production
     }),
     AngularFirestoreModule,
